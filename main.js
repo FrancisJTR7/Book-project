99d9bf6a1ed0c85dcad089f11ec60a8a87445600
FrancisJTR7 <xgalasko@outlook.com>
2023-05-10 17:04:09 -0400

    Added function which adds objects to array

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 let myLibrary = [];
 
-function book(name, pages, read) {
-  this.name = name;
-  this.page = page;
-  this.read = read;
+//Function creates new object based on user input
+function addBook() {
+  // Get user input through a prompt
+  const name = prompt('Enter book name:');
+  const page = parseInt(prompt('Enter amount of pages in book'));
+  // Create a new object with the user input
+  const book = {
+    name: name,
+    page: page,
+  };
+  // Add the new object to myLibrary array
+  myLibrary.push(book);
+  // Log the new object and the updated array to the console
+  console.log(book);
+  console.log(myLibrary);
 }
 
-function addBook() {}
-
-const book1 = new book('Harry Potter', '256', 'not read');
-
-console.log(book1());
+// Add a click event listener to the button
+const button = document.getElementById('add');
+button.addEventListener('click', addBook);
